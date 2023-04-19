adbd9be2306dd5592f4c9a5294646d20d0671ce6
cyber_octopus <gsy@gsy123mailbox.com>
2023-04-19 09:28:41 +0800

    finished lab2

--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -6,5 +6,8 @@
 
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+    unsigned msb = (1u & *reg) ^ (1u & *reg >> 2) ^ (1u & *reg >> 3) ^ (1u & *reg >> 5);
+    *reg = *reg >> 1;
+    *reg |= msb << 15; 
 }
 
